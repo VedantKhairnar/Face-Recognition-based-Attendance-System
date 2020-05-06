a5770101a1e8ce88a7b6dfd4e0eaf0c5ef5de960
Vedant Khairnar <vedron007@gmail.com>
2020-05-06 09:59:32 +0530

    Update attendance.py

--- attendance.py
+++ attendance.py
@@ -1,25 +1,44 @@
+import datetime
 from datetime import datetime
 
 import cv2
 import face_recognition
 import numpy as np
+import openpyxl
 
-video_capture = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+
+def addInExcel(d):
+    fp = "Attendance.xlsx"
+    wb = openpyxl.load_workbook(fp)
+    sheet = wb.get_active_sheet()
+
+    max_row = sheet.max_row
+    max_column = sheet.max_column
+
+    now = datetime.now()
+    # print("Data Acquired :", d)
+    sheet.cell(row=1, column=max_column + 1).value = now.strftime("%m/%d/%Y, %H:%M:%S")
+    for index in range(1, max_row):
+        sheet.cell(row=index + 1, column=max_column + 1).value = d[sheet.cell(row=index + 1, column=2).value]
+    wb.save(fp)
+
+
+video_capture = cv2.VideoCapture(0)
 
 obama_image = face_recognition.load_image_file("images/obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
-biden_image = face_recognition.load_image_file("images/gates.jpg")
-biden_face_encoding = face_recognition.face_encodings(biden_image)[0]
+gates_image = face_recognition.load_image_file("images/gates.jpg")
+gates_face_encoding = face_recognition.face_encodings(gates_image)[0]
 
 known_face_encodings = [
     obama_face_encoding,
-    biden_face_encoding
+    gates_face_encoding
 ]
 known_face_names = [
     "Barack Obama",
     "Bill Gates"
 ]
-attendance_list = {"Barack Obama": "Absent", "Bill Gates": "Absent"}
+attendance_list = {"Barack Obama": "Absent", "Bill Gates": "Absent", " ": " "}
 face_locations = []
 face_encodings = []
 face_names = []
@@ -66,11 +85,40 @@ while True:
     # Hit 'esc' on the keyboard to quit!
     k = cv2.waitKey(30) & 0xff
     if k == 27:
+        present = 0
+        absent = 0
+        print("------------------------------------------")
         print("Attendance as per", datetime.now())
         print("Name  Absent/Present")
         for i in attendance_list:
             print(i, ":", attendance_list[i])
+
+            if attendance_list[i] == "Present":
+                present += 1
+            elif attendance_list[i] == "Absent":
+                absent += 1
+        print("Total Present :", present)
+        print("Total Absent :", absent)
+        print("------------------------------------------")
+        attendance_list['Total Present'] = present
+        attendance_list['Total Absent'] = absent
+
+        # print(attendance_list)
+        addInExcel(attendance_list)
         break
 
 cv2.destroyAllWindows()
 video_capture.release()
+
+"""
+------------------------------------------
+Attendance as per 2020-05-06 09:48:11.096916
+Name  Absent/Present
+Barack Obama : Present
+Bill Gates : Absent
+  :  
+Total Present : 1
+Total Absent : 1
+------------------------------------------
+
+"""
